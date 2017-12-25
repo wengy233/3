869789b719fcca2220d895eb140e999bdf8db197
wengy233 <1197661594@qq.com>
2017-12-25 08:27:39 +0800

    Initial Commit

---
+++ 3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  3
+//
+//  Created by 翁月 on 2017/12/25.
+//  Copyright © 2017年 翁月. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
